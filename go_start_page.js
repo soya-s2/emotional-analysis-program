d593b5a9fe93f01b48cd19e740fe62315da8a96d
soya-s2 <soya.s2.dev@gmail.com>
2021-07-13 22:59:27 +0900

    Add file related to initial access page and QR code scanner

---
+++ go_start_page.js
@@ -0,0 +1,8 @@
+const home_button = document.querySelector('.home_button');
+
+var go_start_page = function () {
+    setTimeout(function () {
+        location.href = 'start_page.html';
+    }, 10)
+};
+home_button.addEventListener('click', go_start_page);
---
+++ start_page.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  img_rotate();
+}
+const img = document.querySelector('.lab_img');
+const QR_button = document.querySelector('.QR_button');
+
+var img_rotate = function () {
+  var ROTATE_CLASS = 'rotate';
+  
+  const hasRotateClass = img.classList.contains(ROTATE_CLASS);
+  if (!hasRotateClass) {
+    img.classList.add(ROTATE_CLASS);
+    setTimeout(function () {
+      img.classList.remove(ROTATE_CLASS);
+    }, 1300);
+  } 
+};
+
+var go_QR = function () {
+  setTimeout(function () {
+    window.location.href = 'qrcode.html';
+  }, 1000);
+}
+img.addEventListener('click', img_rotate);
+QR_button.addEventListener('click', go_QR);
