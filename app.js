087b7e1a742b4413c2014f4aba4b5fca5b09a54c
soya-s2 <soya.s2.dev@gmail.com>
2021-08-09 01:14:07 +0900

    Revise output.html part

--- app.js
+++ app.js
@@ -177,65 +177,61 @@ app.post('/output.html', (request, response) => {
     for (var i = 0; i < property.length; i++) {
         product_list[property[i]] = results[0][Object.keys(results[0])[i + 1]];
     }
-    console.log(sql);
-    console.log(request.body.result);
     const product = product_list;
-    console.log(product);
-    const template = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>output.html</title>
-  <link href="./css/output.css" type="text/css" rel="stylesheet" />
-  <script src="./js/output.js"></script>
-</head>
-<body>
-  <div class="container">
-    <div class="main">
-      <div class="p_name">
-        <h1 class="name"> ${product.name} </h1>
-      </div>
-      <div class="p_img">
-        <img class="img" src="./images/test.jpg"/>
-      </div>
-      <button class="basic_btn"> 기본정보 </button>
-      <div class="p_basic">
-        <h1 class="capacity_and_price"> ${product.capacity}g / ${product.price}원 </h1>
-      </div>
-    </div>
-    <div class="other">
-      <button class="detail_btn"> 상세정보 </button>
-      <div class="p_detail">
-        <div class="first">
-          <h1 class="carbohydarate"> 탄수화물 <br> ${product.carbohydrate}</h1>
-          <h1 class="protein"> 단백질 <br>  ${product.protein}</h1>
-          <h1 class="fat"> 지방 <br>  ${product.fat}</h1>
+    const template = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <title>output.html</title>
+      <link href="./css/output.css" type="text/css" rel="stylesheet" />
+      <script src="./js/output.js"></script>
+      <script src="./js/nutrient.js"></script>
+    </head>
+    <body>
+      <div class="container">
+        <div class="main">
+          <div class="p_name">
+            <h1 class="name"> ${product.name}. </h1>
+          </div>
+          <div class="p_img">
+            <img class="img" src="./images/test.jpg"/>
+          </div>
+          <button class="basic_btn"> 기본정보 </button>
+          <div class="p_basic">
+            <h1 class="capacity_and_price"> ${product.capacity}g / ${product.price}원 </h1>
+          </div>
         </div>
-        <div class="second">
-          <h1 class="kacl"> 칼로리 <br>  ${product.kcal}</h1>
-          <h1 class="salt"> 나트륨 <br>  ${product.salt}</h1>
-          <h1 class="sugar"> 당 <br>  ${product.sugar}</h1>
-        </div>
-        <div class="third">
-          <h1 class="calcium"> 칼슘 <br>  ${product.calcium}</h1>
-          <h1 class="potassium"> 칼륨 <br>  ${product.potassium}</h1>
-          <h1 class="cholesterol"> 콜레스테롤 <br>  ${product.cholesterol}</h1>
+        <div class="other">
+          <button class="detail_btn"> 상세정보 </button>
+            <div class="p_detail grid_container">
+              <h1 class="carbohydarate item"> 탄수화물 <p> ${product.carbohydrate}</h1>
+              <h1 class="protein item"> 단백질 <p>  ${product.protein}</h1>
+              <h1 class="fat item"> 지방 <p>  ${product.fat}</h1>
+    
+              <h1 class="kcal item"> 칼로리 <p>  ${product.kcal}</p></h1>
+              <h1 class="salt item"> 나트륨 <p>  ${product.salt}</h1>
+              <h1 class="sugar item"> 당 <p>  ${product.sugar}</h1>
+    
+              <h1 class="calcium item"> 칼슘 <p>  ${product.calcium}</h1>
+              <h1 class="potassium item"> 칼륨 <p> ${product.potassium}</h1>
+              <h1 class="cholesterol item"> 콜레스테롤 <p> ${product.cholesterol}</h1>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-</body>
-</html>
+    </body>
+    </html>
   `;
     
   response.send(template);
   });
 });
 
+/*
 app.post('/output.html', (request, response) => {
   console.log(request.body);
 });
+*/
 
 app.get('/js/manifest.json', (request, response) => {
   response.sendFile(__dirname + '/js/' + 'manifest.json');
@@ -259,6 +255,10 @@ app.post('/start_page.html', (request, response) => {
 
 app.post('/js/start_page.js', (request, response) => {
   response.sendFile(__dirname + '/js/' + 'start_page.js');
+});
+
+app.get('/js/nutrient.js', (request, response) => {
+  response.sendFile(__dirname + '/js/' + 'nutrient.js');
 });
 
 app.listen(port, () => {
