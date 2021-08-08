0ddd4a41f285acfd51eea71405fac408ece2e58d
soya-s2 <soya.s2.dev@gmail.com>
2021-08-09 01:14:46 +0900

    Add click event

--- js/middle.js
+++ js/middle.js
@@ -5,15 +5,39 @@ window.onload = function () {
 
     
   const no_button = document.querySelector("#reset");
-  console.log(no_button);
+  const yes_button = document.querySelector("#submit");
     
   var go_start_page = function () {
     setTimeout(function () {
       location.href = 'start_page.html';
       console.log("아니오 버튼 클릭");
+      var CLICK_CLASS = 'click';
+    
+    const hasClickClass = no_button.classList.contains(CLICK_CLASS);
+    if (!hasClickClass) {
+      no_button.classList.add(CLICK_CLASS);
+    }
+    setTimeout(function () {
+      no_button.classList.remove(CLICK_CLASS);
+    }, 3000);
     }, 10)
   };
   no_button.addEventListener('click', go_start_page);
+
+  var click_submit = function () {
+    var CLICK_CLASS = 'click';
+    
+    const hasClickClass = yes_button.classList.contains(CLICK_CLASS);
+    if (!hasClickClass) {
+      yes_button.classList.add(CLICK_CLASS);
+    }
+    setTimeout(function () {
+      yes_button.classList.remove(CLICK_CLASS);
+    }, 3000);
+  };
+  
+  yes_button.addEventListener('click', click_submit);
 };
+
 
 
