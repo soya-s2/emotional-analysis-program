b268a1d39f4e6ac643e8db70e2d3227a1ec99ae2
soya-s2 <soya.s2.dev@gmail.com>
2021-08-26 23:21:34 +0900

    Add image cognition algorithm for album

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-﻿/*전역변수*/
-//lightFlag - 손전등 on-true/off-false
-//stop - setTimeout 반환값 (clearTimeout 호출시 사용)
+﻿/* 전역변수 */
+// lightFlag - 손전등 on-true/off-false
+// stop - setTimeout 반환값 (clearTimeout 호출시 사용)
 var lightFlag;
 var stop;
 
-/*창 로드시 실행*/
+/* 창 로드시 실행 */
 window.onload = function () {
   var os = getMobileOperatingSystem();
-  /*os 구별 및 손전등 사용 가능여부 판단*/
+  /* os 구별 및 손전등 사용 가능여부 판단 */
   if (os === "iOS" || os === "unknown") {
   } else {
     document.querySelector("#lightBtn").style.display = "inline-block";
@@ -69,7 +69,7 @@ window.onload = function () {
     });
 };
 
-/*실시간 video snapshot & qrcode판단*/
+/* 실시간 video snapshot & qrcode판단 */
 function findQR() {
   var next = false;
   var next_cnt = 0;
@@ -88,10 +88,8 @@ function findQR() {
       qrCanvasElement.height
     );
     try {
-      next_cnt++;
-      next = true;
-      if (next && next_cnt === 1) {
-        result = qrcode.decode();
+      result = qrcode.decode();
+      if (result) {
         // query(쿼리문, 콜백함수)
         window.location.href = "./middle.html";
         console.log("값 : " + result);
@@ -99,17 +97,18 @@ function findQR() {
         localStorage.setItem("QR_result", result);
       }
     } catch (e) {
-      /* 인식 못한 경우 */
+       /* 인식 못한 경우 */
+
     }
   }
-  /*재귀 호출*/
+  /* 재귀 호출 */
   stop = setTimeout(findQR, 100);
 }
 
-/*손전등 버튼 선택시 실행*/
+/* 손전등 버튼 선택시 실행 */
 function setLight() {
   var os = getMobileOperatingSystem();
-  /*Android, Windows Phone에서만 실행*/
+  /* Android, Windows Phone에서만 실행 */
   if (os === "Android" || os === "Windows Phone") {
     var video = document.getElementById("video-preview");
     const track = video.srcObject.getVideoTracks()[0];
@@ -134,56 +133,54 @@ function setLight() {
   }
 }
 
-/*앨범에서 이미지 선택 후 실행*/
+/* 앨범에서 이미지 선택 후 실행 */
 // input : 호출한 input태그 전체
 function previewFile(input) {
-  /*멈추기*/
+  /* 멈추기 */
   clearTimeout(stop);
 
-  /*파일 읽기*/
+  /* 파일 읽기 */
   var file = document.querySelector("#album_file");
   var fileList = file.files;
   var reader = new FileReader();
   reader.readAsDataURL(fileList[0]);
 
-  /*앨범 이미지 임시저장*/
+  /* 앨범 이미지 임시저장 */
   var tmpImage = new Image();
-  /*파일 로드 후 실행*/
+  /* 파일 로드 후 실행 */
   reader.onload = function (input) {
-    /*이미지 띄우기*/
+    /* 이미지 띄우기 */
     document.querySelector("#image_section").style.display = "inline-block";
     document.querySelector("#image_section").src = reader.result;
     setTimeout(function () {
       tmpImage.src = reader.result;
-    }, 200); //이미지를 화면에 띄운 뒤 알람창 띄우기 위해 delay사용
+    }, 200); // 이미지를 화면에 띄운 뒤 알람창 띄우기 위해 delay사용
   };
 
-  /*이미지 임시저장 후 qrcode판단*/
+  /* 이미지 임시저장 후 qrcode판단 */
   tmpImage.onload = function () {
     var qrCanvasElement = document.getElementById("qr-canvas");
     var qrCanvas = qrCanvasElement.getContext("2d");
+    var image_section = document.querySelector("#image_section");
+
     qrCanvasElement.width = tmpImage.width;
     qrCanvasElement.height = tmpImage.height;
+    qrCanvasElement.style.display = "none";
     qrCanvas.drawImage(tmpImage, 0, 0, tmpImage.width, tmpImage.height);
     try {
-      var result = qrcode.decode();
-      console.log(result);
-      next_cnt++;
-      next = true;
-
-      if (next && next_cnt === 1) {
+      result = qrcode.decode();
+      if (result) {
         // query(쿼리문, 콜백함수)
         window.location.href = "./middle.html";
         console.log("값 : " + result);
         console.log("타입 : " + typeof result);
         localStorage.setItem("QR_result", result);
-        console.log(result);
       }
     } catch (e) {
-      /* 인식 못한 경우 */
-      alert("인식하지 못하였습니다. 다시 시도해주세요.");
-      document.querySelector("#image_section").style.display = "none";
-      setTimeout(findQR, 100);
+        /* 인식 못한 경우 */
+        alert("인식하지 못하였습니다. 다시 시도해주세요.");
+        document.querySelector("#image_section").style.display = "none";
+        setTimeout(findQR, 100);
     }
-  };
+}
 }
